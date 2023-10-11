6cf0dbc380c0c7e2d4ed46d0229143b527661db1
Yunus Emre Mersinli <86349634+Ceryunus@users.noreply.github.com>
2023-10-11 11:59:34 +0300

    Add files via upload

--- hand_dedect_live.py
+++ hand_dedect_live.py
@@ -12,11 +12,13 @@ os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 class Hand_dedector:
     def __init__(self) -> None:
 
-        self.model = YOLO("runs/detect/train/weights/best.pt")#eğitilmi modeli yükleme
-        self.cap = cv2.VideoCapture(0)#kamera
-        self.rcords = []#"rounded cords" yuvarlanmış kordinatlar
-        self.h=480
-        self.w=640
+        # [Tr] eğitilmiş modeli yükleme [En] loading the trained model
+        self.model = YOLO("runs/detect/train/weights/best.pt")
+        self.cap = cv2.VideoCapture(0)  # [Tr] kamera [En] camera
+        self.rcords = []  # [Tr] "rcords = rounded cords" yuvarlanmış kordinatlar [En] Rounded cords
+        self.h = 480
+        self.w = 640
+
     def get_cropped_frame(self):
         _, frame = self.cap.read()
 
@@ -25,32 +27,26 @@ class Hand_dedector:
         results = self.model.predict(
             source=frame, save=False, stream=True, show=False)
         self.cords = []
-        # tahmin edilen kutunun kordinatlarını alma.
+        # [Tr] tahmin edilen kutunun kordinatlarını alma.[En] get the coordinates of the predicted box.
         for r in results:
 
             annotator = Annotator(frame)
 
             boxes = r.boxes
             for box in boxes:
-                #    left   top   right    buttom
-                # get box coordinates in (top, left, bottom, right) format
                 b = box.xyxy[0]
                 self.cords = b.tolist()
                 self.rcords = [round(num) for num in self.cords]
                 c = box.cls
                 annotator.box_label(b, self.model.names[int(c)])
 
-        # Resmi kırpma işlemi
+        # [Tr] [Tr]Resmi kırpma işlemi [En] Crop image
         if self.rcords:
-                        # Resmin boyutlarını alın
-            
-
-            # Kırpılacak bölgenin koordinatlarını hesaplayın ve sınırları kontrol edin
             y1 = max(self.rcords[1]-40, 0)
             y2 = min(self.rcords[3]+40, self.h)
             x1 = max(self.rcords[0]-40, 0)
             x2 = min(self.rcords[2]+40, self.w)
-            # Resmi kırpın
+            # [Tr] Resmi kırpın
             cropped_img = frame[y1:y2, x1:x2]
             # cropped_img = frame[self.rcords[1]-40:self.rcords[3] +
             #                     40, self.rcords[0]-40:self.rcords[2]+40]
--- hand_dedector_train.py
+++ hand_dedector_train.py
@@ -3,8 +3,5 @@ import os
 
 if __name__ == '__main__':
     os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
-    # Load a model
-    model = YOLO("yolov8n.yaml")  # build a new model from scratch
-
-    # Use the model
+    model = YOLO("yolov8n.yaml")  
     model.train(data="config.yaml", epochs=10)  # train the model
